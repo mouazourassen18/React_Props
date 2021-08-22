6d564a8f963baceaf0290a2d4cde59d76aecb333
mouazourassen18 <54610297+mouazourassen18@users.noreply.github.com>
2021-08-22 08:20:38 -0400

    2d pic

--- src/index.js
+++ src/index.js
@@ -18,7 +18,10 @@ ReactDOM.render(
       img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
     />{" "}
     ,
-    <Card name="Koloooo" />
+    <Card
+      name="Tom Hanks"
+      img="https://media.npr.org/assets/img/2021/06/11/tom-hanks-29d69e8c39cd01d67c1a36d0a81674923d18577f.jpg"
+    />
   </div>,
   document.getElementById("root")
 );
